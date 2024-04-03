f850a680a5582b0daec03761f6d38550830f8fcb
Sean <seanreilly123@hotmail.com>
2024-04-03 11:39:55 +1100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDlRxLAu5k_Nl2nq6M9C7Xxmr9ZjH81HnA",
+    authDomain: "choir-emails.firebaseapp.com",
+    projectId: "choir-emails",
+    storageBucket: "choir-emails.appspot.com",
+    messagingSenderId: "3841914771",
+    appId: "1:3841914771:web:e95adefa1f94c13f5c5a66",
+    measurementId: "G-7Y6NDRRE0X",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div className="App">EMails</div>;
 }
 
 export default App;
